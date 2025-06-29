21d0103dff60ddf6da53d4c85e248db54cc3b738
Luiz Felipe Alves Rodrigues <luizrodriguescode@gmail.com>
2025-06-29 17:04:38 -0300

    init: Inicializacao do app

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:chat_app/pages/onboarding.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ChatApp());
+}
+
+class ChatApp extends StatefulWidget {
+  const ChatApp({super.key});
+
+  @override
+  State<ChatApp> createState() => _ChatAppState();
+}
+
+class _ChatAppState extends State<ChatApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: OnboardingPage());
+  }
+}
